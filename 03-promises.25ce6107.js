a491abfa826479a91f57dd29c1cad112cea0ddbd
GeorgiySergeev <126503204+GeorgiySergeev@users.noreply.github.com>
2023-07-31 10:58:36 +0000

    Deploying to gh-pages from @ GeorgiySergeev/goit-js-hw-09@31067a26fb80c7e444036243baa96e43580956e3 🚀

--- 03-promises.25ce6107.js
+++
@@ -1,2 +0,0 @@
-new Promise((function(o,n){o(console.log("Ok!")),n(console.log("No!"))}));
-//# sourceMappingURL=03-promises.25ce6107.js.map
--- 03-promises.321fcc32.js
+++
@@ -1,2 +0,0 @@
-new Promise(((o,e)=>{o(console.log("Ok!")),e(console.log("No!"))}));
-//# sourceMappingURL=03-promises.321fcc32.js.map
